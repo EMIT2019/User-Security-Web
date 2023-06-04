7789e7fbd5554a2673c00ca741de08b43d9ba96f
EMIT2019 <emilianoandreurbinazavala@gmail.com>
2023-06-03 21:46:35 -0600

    Sing Out Option Added

--- js/functionality/dashboard_profile_info.js
+++ js/functionality/dashboard_profile_info.js
@@ -1,6 +1,13 @@
 let profile_box = document.getElementById('profile_info_container');
+let btn_sing_out = document.getElementById('btn_sing_out');
+
 let full_name = () => {
     return `${JSON.parse(localStorage.getItem('user')).name} ${JSON.parse(localStorage.getItem('user')).lastname}`;
 };
 
 profile_box.firstElementChild.innerHTML = full_name();
+
+btn_sing_out.addEventListener("click", () => {
+    localStorage.removeItem('user');
+    location.href = '../login.html';
+});
